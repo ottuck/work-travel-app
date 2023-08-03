a8a8585ea80f756af33cdfcbf049bfa37b76bf9b
SangHyun Kim <vison901@gmail.com>
2023-08-03 22:51:26 +0900

    impliment curd feature

--- App.js
+++ App.js
@@ -1,12 +1,61 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { FontAwesome } from '@expo/vector-icons';
 import { theme } from './colors';
+
+const STORAGE_KEY = "@toDos";
 
 export default function App() {
   const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+
+  const saveToDos = async (toSave) => {
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave));
+    //Json Object객체는 String형으로 parsing해서 넣어주기
+  };
+  const loadToDo = async (toLoad) => {
+      const s = await AsyncStorage.getItem(STORAGE_KEY);
+      if(s){
+        setToDos(JSON.parse(s));
+      }
+  };
+  useEffect(() => {
+    loadToDo();
+  }, []);
+
+  const addToDo = async () => {
+    if (text === "") {
+      return;
+    }
+    const newToDo = {
+      ...toDos,
+      [Date.now()]: { text, working }
+    };
+    setToDos(newToDo);
+    await saveToDo(newToDo);
+    setText("");  //검색창 비워주기
+  };
+  const deleteToDo = (key) => {
+    Alert.alert("Delete To Do", "Are you sure?", [
+      { text: "Cancel" },
+      {
+        text: "I'm Sure",
+        style: "destructive",
+        onPress: () => {
+          const newToDos = { ...toDos };
+          delete newToDos[key];
+          setToDos(newToDos);
+          saveToDos(newToDos);
+        },
+      },
+    ]);
+  };
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -27,8 +76,28 @@ export default function App() {
         </TouchableOpacity>
       </View>
       <View>
-        <TextInput placeholder={working ? "이건 해야만 해! 🕺🏻" : "어디로 떠나고 싶으세요? 🏖️"} style={styles.input} />
+        <TextInput
+          onSubmitEditing={addToDo} //제출 form 역할
+          value={text}
+          onChangeText={onChangeText}
+          //실시간으로 받는 역할 -> onchange함수에서 setText해줌
+          returnKeyType="done"
+          placeholder={working ? "이건 해야만 해! 🕺🏻" : "어디로 떠나고 싶으세요? 🏖️"}
+          style={styles.input}
+        />
       </View>
+      <ScrollView>
+        {Object.keys(toDos).map((key) => (
+          toDos[key].working === working ?
+            <View style={styles.toDo} key={key}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+              <TouchableOpacity onPress={() => { deleteToDo(key) }}>
+                <FontAwesome name="trash" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+            : null
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -55,6 +124,22 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     borderRadius: 30,
     marginTop: 20,
+    marginVertical: 20,
     fontSize: 18,
+  },
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  toDoText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "500",
   }
 });
